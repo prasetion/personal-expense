30857b8c2b7fcc3f678e8891b0765c71b645b8e4
prasetion <prasetion@gmail.com>
2022-04-02 18:39:27 +0700

    composition component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
 const Expenses = ({ expenses }) => {
   return (
-    <div>
+    <Card className="expenses">
       <ExpenseItem props={expenses[0]}></ExpenseItem>
       <ExpenseItem props={expenses[1]}></ExpenseItem>
       <ExpenseItem props={expenses[2]}></ExpenseItem>
       <ExpenseItem props={expenses[3]}></ExpenseItem>
-    </div>
+    </Card>
   );
 };
 
